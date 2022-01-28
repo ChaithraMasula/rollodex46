b51f1fe91566804ac46938bb7782bd77270c3318
Chaithra Masula <chaithra.masula@qentelli.com>
2022-01-28 17:51:59 +0530

    inserting file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Home from './pages/Home/Home'
 import './index.css'
 import {Provider} from 'react-redux'
 import store from './redux/store'
+import {BrowserRouter} from 'react-router-dom'
+import App from './App'
 
 ReactDOM.render(
-    <>
+    <> 
+    {/* provider connecting bridge between react component Home and redux store */}
         <Provider store = {store}>
-            <Home/>
+            <BrowserRouter>
+                <App/>
+             </BrowserRouter>
         </Provider>
+
     </>
     ,document.getElementById('root'))
